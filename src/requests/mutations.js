454175be45a5622c00a39484a8375b960f3fc70c
jsina <jsina.maleki@gmail.com>
2018-08-25 13:23:28 +0430

    add patch  mutation

--- src/requests/mutations.js
+++ src/requests/mutations.js
@@ -1,25 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
-export function addUser({firstname, age, companyId}) {
-    return axios.post('http://localhost:4000/users', {
-        firstname,
-        age,
-        companyId,
-    })
+export function addUser({ firstname, age, companyId }) {
+  return axios.post("http://localhost:4000/users", {
+    firstname,
+    age,
+    companyId
+  });
 }
 
 export function addUsers(users) {
-    const promises = users.map(({firstname, age, companyId}) =>
-        axios.post('http://localhost:4000/users', {
-            firstname,
-            age,
-            companyId
-        })
-    );
-    return Promise.all(promises)
+  const promises = users.map(({ firstname, age, companyId }) =>
+    axios.post("http://localhost:4000/users", {
+      firstname,
+      age,
+      companyId
+    })
+  );
+  return Promise.all(promises);
 }
 
 export function deleteUser(userId) {
-    console.log(userId)
-    return axios.delete(`http://localhost:4000/users/${userId}`);
+  return axios.delete(`http://localhost:4000/users/${userId}`);
+}
+
+export function updateUser(id, user) {
+  return axios.patch(`http://localhost:4000/users/${id}`, user);
 }
